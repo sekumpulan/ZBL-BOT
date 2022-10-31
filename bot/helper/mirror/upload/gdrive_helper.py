209cd5c8da90b18341d5a9e500e467bd7fa634ea
Sekumpulan TxT <116649984+sekumpulan@users.noreply.github.com>
2022-10-31 13:25:10 +0700

    Update gdrive_helper.py

--- bot/helper/mirror/upload/gdrive_helper.py
+++ bot/helper/mirror/upload/gdrive_helper.py
@@ -213,7 +213,7 @@ class GoogleDriveHelper:
         # File body description
         file_metadata = {
             "name": file_name,
-            "description": "Uploaded by Mirror-leech-telegram-bot",
+            "description": "Diunggah oleh ZBL Bot",
             "mimeType": mime_type,
         }
         if parent_id is not None:
@@ -543,7 +543,7 @@ class GoogleDriveHelper:
     def __create_directory(self, directory_name, parent_id):
         file_metadata = {
             "name": directory_name,
-            "description": "Uploaded by Mirror-leech-telegram-bot",
+            "description": "Diunggah oleh ZBL Bot",
             "mimeType": self.__G_DRIVE_DIR_MIME_TYPE,
         }
         if parent_id is not None:
